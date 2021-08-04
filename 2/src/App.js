7db1ad6893039e32a255826c1b7c98dafbb1ce12
LustLoop <lustloop@gmail.com>
2021-08-04 18:28:04 +0300

    Basic html layout

--- 2/src/App.js
+++ 2/src/App.js
@@ -1,11 +1,33 @@
 import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <p>test</p>
-    </div>
-  );
+class App extends Component {
+    render () {
+        return (
+            <div className="container">
+                <h1>Stackfindover</h1>
+                <form className="login-form">
+                    <h2>Sign in your account</h2>
+                    <label>
+                        Email
+                        <input className="field-input" type="email" />
+                    </label>
+                    <label>
+                        Password
+                        <p>Forgot your password?</p>
+                        <input className="field-input" type="password" />
+                    </label>
+                    <label>
+                        <input type="checkbox" />
+                        Stay signed in for a week
+                    </label>
+                    <button>Continue</button>
+                    <p>Use single sign-on (Google) instead</p>
+                    <p>Don't have an account? <a href="/">Sign up</a></p>
+                </form>
+            </div>
+        )
+    }
 }
 
 export default App;
