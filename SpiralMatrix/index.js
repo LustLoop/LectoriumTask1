50173fbf2fb1df44ec1d5037eb51190081de2904
LustLoop <lustloop@gmail.com>
2021-09-17 00:39:25 +0300

    Add basic iteration logic

--- SpiralMatrix/index.js
+++ SpiralMatrix/index.js
@@ -1,5 +1,6 @@
 function moveSpirally(matrixWidth, matrixHeight, horizontalStartPoint, verticalStartPoint, initDirection) {
-    createMatrix(matrixWidth, matrixHeight)
+    const matrix = createMatrix(matrixWidth, matrixHeight)
+    startIterating(matrixWidth, matrixHeight, horizontalStartPoint - 1, verticalStartPoint - 1, 'left')
 }
 
 function createMatrix(matrixWidth, matrixHeight) {
@@ -14,6 +15,55 @@ function createMatrix(matrixWidth, matrixHeight) {
         matrix.push(line);
     }
     return matrix;
+}
+
+function testqwe(matrix) {
+    console.log(matrix[4-1][2-1])
+}
+
+function startIterating(matrixWidth, matrixHeight, currentHorizontalIndex, currentVerticalIndex, initialDirection) {
+    let stepsLeft = matrixWidth * matrixHeight;
+    do {
+        switch (initialDirection) {
+            case 'right':
+                moveRight();
+            case 'down':
+                moveDown();
+            case 'left':
+                moveLeft();
+            case 'up':
+                moveUp();
+        }
+    } while (stepsLeft !== 0) {
+        moveRight(currentHorizontalIndex, currentVerticalIndex);
+        moveDown(currentHorizontalIndex, currentVerticalIndex);
+        moveLeft(currentHorizontalIndex, currentVerticalIndex);
+        moveUp(currentHorizontalIndex, currentVerticalIndex);
+    }
+
+    function moveLeft() {
+        increaseCounter();
+    }
+
+    function moveUp() {
+        increaseCounter();
+    }
+
+    function moveRight() {
+        increaseCounter();
+    }
+
+    function moveDown() {
+        increaseCounter();
+    }
+
+    function increaseCounter() {
+        if (stepsLeft === 0) {
+            return;
+        }
+        console.log(stepsLeft)
+        stepsLeft--;
+    }
 }
 
 moveSpirally(5, 6, 2, 4, 'left')
