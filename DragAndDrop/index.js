2ec7e40332e12a35019a618b96aa6c51b41cb4fd
Volodymyr Yesypenko <volodymyryesypenko@gmail.com>
2021-10-27 23:33:19 +0300

    Fix block in the interactive field

--- DragAndDrop/index.js
+++ DragAndDrop/index.js
@@ -1,6 +1,6 @@
 let field = document.querySelector('#interactive-field');
 let savedBlock = localStorage.getItem("block");
-const newBlock = '<div id="draggable-block"><p id="block-name">Default name</p></div>';
+const newBlock = '<div id="draggable-block" style="position: absolute"><p id="block-name" >Default name</p></div>';
 let block = document.querySelector('#draggable-block');
 let nameField = document.querySelector('#block-name');
 
@@ -8,6 +8,8 @@ window.onload = (e) => {
     if (savedBlock != null) {
         field.innerHTML = savedBlock;
         block = document.querySelector('#draggable-block');
+        block.style.left = localStorage.getItem("shiftX");
+        block.style.top = localStorage.getItem("shiftY");
         nameField = document.querySelector('#block-name');
         setBlockQualities();
     }
@@ -19,48 +21,78 @@ field.addEventListener('dblclick', function (e) {
         field.innerHTML = savedBlock;
         localStorage.setItem("block", newBlock);
         block = document.querySelector('#draggable-block');
+        block.style.left = e.pageX + 'px';
+        block.style.top = e.pageY + 'px';
+        localStorage.setItem("shiftX", block.style.left);
+        localStorage.setItem("shiftY", block.style.top);
         setBlockQualities();
     }
 });
 
 function setBlockQualities() {
-    nameField.onclick = function (e) {
-        console.log('click')
-    }
 
     block.onmousedown = function(e) {
-        let coords = getCoords(block);
-        let shiftX = e.pageX - coords.left;
-        let shiftY = e.pageY - coords.top;
-
-        console.log(shiftX);
+        const fieldCoords = field.getBoundingClientRect()
+        let blockCoords = getCoords(block);
+        let shiftX = e.pageX - blockCoords.left;
+        let shiftY = e.pageY - blockCoords.top;
 
         block.style.position = 'absolute';
         moveAt(e);
 
         block.style.zIndex = 1000;
 
-        function moveAt(e) {
-            block.style.left = e.pageX - shiftX + 'px';
-            block.style.top = e.pageY - shiftY + 'px';
-        }
-
         document.onmousemove = function(e) {
             moveAt(e);
         }
 
-        block.onmouseup = function() {
+        block.onmouseup = function(e) {
+            const blockWidth = blockCoords.right - blockCoords.left;
+            const blockHeight = blockCoords.bottom - blockCoords.top;
+
+            blockCoords = getCoords(block);
+
+            console.log(blockCoords)
+            console.log(fieldCoords)
+            console.log(blockHeight)
+
+            if (blockCoords.left < fieldCoords.left) {
+                block.style.left = fieldCoords.left + 'px';
+            }
+
+            if (blockCoords.right > fieldCoords.right) {
+                block.style.left = fieldCoords.right - blockWidth + 'px';
+            }
+
+            if (blockCoords.top < fieldCoords.top) {
+                block.style.top = fieldCoords.top + 'px';
+            }
+
+            if (blockCoords.bottom > fieldCoords.bottom) {
+                block.style.top = fieldCoords.bottom - blockHeight + 'px';
+            }
+
+            localStorage.setItem("shiftX", block.style.left);
+            localStorage.setItem("shiftY", block.style.top);
+
             document.onmousemove = null;
             block.onmouseup = null;
         }
+
+        function moveAt(e) {
+            block.style.left = e.pageX - shiftX + 'px';
+            block.style.top = e.pageY - shiftY + 'px';
+            
+        }
     }
 }
-
 
 function getCoords(elem) {
     let box = elem.getBoundingClientRect();
     return {
         top: box.top + pageYOffset,
-        left: box.left + pageXOffset
+        left: box.left + pageXOffset,
+        bottom: box.bottom,
+        right: box.right
     };
 }
